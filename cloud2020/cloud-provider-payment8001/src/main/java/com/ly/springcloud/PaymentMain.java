dd25011b64450d692f9479be0ee73f3f0c094101
ly10228 <601520157@qq.com>
2020-03-29 16:24:20 +0800

    主启动类

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/ly/springcloud/PaymentMain.java
@@ -0,0 +1,17 @@
+package com.ly.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luoyong
+ *  * @create 2020-03-29 4:22 下午
+ *  * @last modify by [luoyong 2020-03-29 4:22 下午]
+ * @Description: 主配置类
+ **/
+@SpringBootApplication
+public class PaymentMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain.class, args);
+    }
+}
