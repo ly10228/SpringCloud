877e3b076d56c545861518c6e1ac82041f9f96ac
ly10228 <601520157@qq.com>
2020-03-29 16:49:20 +0800

    引入swagger

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/ly/springcloud/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.ly.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author luoyong
+ *  * @create 2020-03-29 4:42 下午
+ *  * @last modify by [luoyong 2020-03-29 4:42 下午]
+ * @Description: SwaggerConfig
+ **/
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ly.springcloud"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("SpringBoot整合Swagger")
+                        .description("SpringBoot整合Swagger，详细信息......")
+                        .version("9.0")
+                        .contact(new Contact("啊啊啊啊", "blog.csdn.net", "aaa@gmail.com"))
+                        .license("The Apache License")
+                        .licenseUrl("http://www.baidu.com")
+                        .build());
+    }
+}
