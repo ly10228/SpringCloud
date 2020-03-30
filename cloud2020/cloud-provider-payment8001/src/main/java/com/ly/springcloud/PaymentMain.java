53e01b83b5394bf9406767d8148832821668e574
ly10228 <601520157@qq.com>
2020-03-30 23:51:38 +0800

    注册provider

--- cloud2020/cloud-provider-payment8001/src/main/java/com/ly/springcloud/PaymentMain.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/ly/springcloud/PaymentMain.java
@@ -2,6 +2,7 @@ package com.ly.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
  **/
 @ComponentScan({ "com.ly.springcloud"})
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain.class, args);
