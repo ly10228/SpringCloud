4964100fe2e348e066c6022545e9bfd5dbcbd7a7
ly10228 <601520157@qq.com>
2020-04-06 17:00:28 +0800

    cloud-consumer-order80注册到Eureka的步骤流程

--- cloud2020/cloud-consumer-order80/src/main/java/com/ly/springcloud/OrderMain.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/ly/springcloud/OrderMain.java
@@ -2,6 +2,7 @@ package com.ly.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author luoyong
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description: OrderMain
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class, args);
